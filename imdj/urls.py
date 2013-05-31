d8a3877af1080ec235ee34d2d432816186c130d8
Marcos Moyano <marcos@anue.biz>
2013-05-31 14:34:25 -0300

    fixtures para todos

--- imdj/urls.py
+++ imdj/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, include, url
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
+
 
 urlpatterns = patterns('',
     url(r'^', include('imdj.movies.urls', namespace='movies')),
@@ -13,3 +15,14 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+
+
+if settings.DEBUG:
+    urlpatterns += patterns(
+        '',
+        (r'^media/(?P<path>.*)$',
+         'django.views.static.serve',
+         {'document_root': settings.MEDIA_ROOT}),
+    )
+
+urlpatterns += staticfiles_urlpatterns()
